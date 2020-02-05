acf474fb851af3818030fe48db57f7b4a4d5f2a0
Eirik Gribbestad Gustafsson <eirikgg94@gmail.com>
2020-02-05 19:00:45 +0100

    Cleaned code, fixed transparent image

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -6,13 +6,16 @@ class gui(object):
     root = None
 
     # Variables for the dividers
-    frame_top = None
-    frame_bot = None
+    frame_left = None
+    frame_right = None
 
     # Variables for the buttons
     btn_center = None
     btn_circle = None
     btn_fig8 = None
+
+    # Video source
+    vid = None
 
     # Setup gui
     def __init__(self):
@@ -21,7 +24,7 @@ class gui(object):
         self.pack_dividers()
         self.create_btns()
         self.pack_btns()
-        self.set_image()
+        self.set_image('640x360.png')
         self.restrict_size()
 
     # Start gui
@@ -32,27 +35,27 @@ class gui(object):
     def set_title(self, title="6DOF-Platform"):
         self.root.title(title)
 
-    # Main dividers devide the gui in several parts
+    # Main dividers divide the gui in several parts
     def create_dividers(self):
-        self.frame_top = Frame(self.root, pady=5, padx=5, highlightbackground="black", highlightthickness=1)
-        self.frame_bot = Frame(self.root, pady=5, padx=5, highlightbackground="black", highlightthickness=1)
+        self.frame_left = Frame(self.root, pady=5, padx=5, highlightbackground="black", highlightthickness=1)
+        self.frame_right = Frame(self.root, pady=5, padx=5, highlightbackground="black", highlightthickness=1)
         
     # Pack dividers
     def pack_dividers(self):
-        self.frame_top.pack(side=RIGHT, pady=5, padx=5, fill = X)
-        self.frame_bot.pack(side=LEFT, pady=5, padx=5)
+        self.frame_right.pack(side=RIGHT, pady=5, padx=5)
+        self.frame_left.pack(side=LEFT, pady=5, padx=5)
 
     # Create buttons
     def create_btns(self):
-        self.btn_center = Button(self.frame_top, text="Center")
-        self.btn_circle = Button(self.frame_top, text="Circle")
-        self.btn_fig8 = Button(self.frame_top, text="Figure 8")
+        self.btn_center = Button(self.frame_right, text="Center")
+        self.btn_circle = Button(self.frame_right, text="Circle")
+        self.btn_fig8 = Button(self.frame_right, text="Figure 8")
 
     # Pack buttons
     def pack_btns(self):
-        self.btn_center.grid(row=0, column=0, pady=5, padx=5)
-        self.btn_circle.grid(row=1, column=0, pady=5, padx=5)
-        self.btn_fig8.grid(row=2, column=0, pady=5, padx=5)
+        self.btn_center.grid(row=0, column=0, pady=5, padx=5, sticky='nsew')
+        self.btn_circle.grid(row=1, column=0, pady=5, padx=5, sticky='nsew')
+        self.btn_fig8.grid(row=2, column=0, pady=5, padx=5, sticky='nsew')
 
     # Restrict minimum size
     def restrict_size(self):
@@ -60,12 +63,13 @@ class gui(object):
         self.root.minsize(self.root.winfo_width(), self.root.winfo_height())
         self.root.maxsize(self.root.winfo_width(), self.root.winfo_height())
 
-    # Add placeholder image
-    def set_image(self):
-        img = Image.open('h.jpg')
-        img = ImageTk.PhotoImage(img)
-        can = Label(self.frame_bot, image = img)
-        can.pack(side = "bottom", fill = "both", expand = "yes")
+    # Set placeholder image
+    def set_image(self, img_path):
+        img = ImageTk.PhotoImage(Image.open(img_path))
+        label = Label(self.frame_left, image = img)
+        label.image = img
+        label.pack(fill = "both", expand = "yes")
+
 
 if __name__ == "__main__":
     gui = gui()
