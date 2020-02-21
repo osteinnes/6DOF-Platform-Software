0760428ec83a6a308aeae3bfc1f9496674906b21
osteinnes <olemartinhs@gmail.com>
2020-02-22 00:42:56 +0100

    Divided into relevant functions and added descriptive comments

--- src/webcam/ball_tracking.py
+++ src/webcam/ball_tracking.py
@@ -48,39 +48,65 @@ def getContourCircle(img):
     blue = cv2.inRange(hsv, (100, 150, 50), (135, 255, 255))
     mask = red + blue
 
-    contours = []
-
+    # Find contours
     contours, hierarchy = cv2.findContours( mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    #print(contours)
-
     if len(contours) > 1:
+        # Sort contours by area (decending)
         contour_sizes = [(cv2.contourArea(contour), contour)
                         for contour in contours]
         biggest_contours = sorted(contour_sizes, key=lambda x: x[0], reverse=True)
-        #biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
         
+        # Retrieve relevant contours
         biggest_contour = biggest_contours[0][1]
         smallest_contour = biggest_contours[2][1]
 
+        # Draw contours on image
         cv2.drawContours(img, smallest_contour, -1, (0, 255, 0), 3)
         cv2.drawContours(img, biggest_contour, -1, (255, 0, 0), 3)
 
-        M = cv2.moments(biggest_contour)
-        M2 = cv2.moments(smallest_contour)
+        # Find and draw centre of contours
+        img = drawContourCenter(biggest_contour, img, (255, 0, 0))
+        img = drawContourCenter(smallest_contour, img, (0, 255, 0))
 
-        if M["m00"] != 0:
+        # Retrieve coordinates of center and print them
+        x,y = getContourCenter(smallest_contour)
+        print("Center of ball>> x:", x, " y: ", y)
+
+    # Return image with contours and centers drawn (if any)
+    return img
+
+# Draws center of a contour
+def drawContourCenter(contour, img, color):
+
+    # Weighted average of image pixel intesities to find centroid of contour (blob)
+    M = cv2.moments(contour)
+
+    # Check if segmentation were successful
+    if M["m00"] != 0:
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
 
-            cv2.circle(img, (cX, cY), 7, (255, 0, 0), -1)
-        if M2["m00"] != 0:
-            cX = int(M2["m10"] / M2["m00"])
-            cY = int(M2["m01"] / M2["m00"])
-
-            cv2.circle(img, (cX, cY), 7, (0, 255, 0), -1)
-
+            cv2.circle(img, (cX, cY), 7, color, -1)
     return img
+
+# Returns center coordinates of a contour
+def getContourCenter(contour):
+
+    # Weighted average of image pixel intesities to find centroid of contour (blob)
+    M = cv2.moments(contour)
+
+    # Initialize coordinates
+    cX = None
+    cY = None
+
+    # Check if segmentation were successful
+    if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+
+    # Return coordinates
+    return cX, cY
 
 
 if __name__ == "__main__":
