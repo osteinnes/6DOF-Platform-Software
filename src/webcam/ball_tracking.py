3b798729debab1c2474a20b76762ba05b36d2f2b
omsteinn <omsteinn@stud.ntnu.no>
2020-02-24 11:07:23 +0100

    Updated ball_tracking.py

--- src/webcam/ball_tracking.py
+++ src/webcam/ball_tracking.py
@@ -49,24 +49,40 @@ def getContourCircle(img):
     mask = red + blue
 
     contours = []
+    areaArray = []
+    count = 1
 
     contours, hierarchy = cv2.findContours( mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
+    
     print(contours)
 
-    if len(contours) > 1:
+    if len(contours) > 2:
         contour_sizes = [(cv2.contourArea(contour), contour)
                         for contour in contours]
+
+        print(contour_sizes)
         biggest_contour = max(contour_sizes, key=lambda x: x[0])[1]
+        next_contour = max(contour_sizes, key=lambda x: x[1])[1]
+
         cv2.drawContours(img, biggest_contour, -1, (0, 255, 0), 3)
+        cv2.drawContours(img, next_contour, -1, (0, 0, 255), 3)
 
         M = cv2.moments(biggest_contour)
+        M2 = cv2.moments(next_contour)
 
         if M["m00"] != 0:
             cX = int(M["m10"] / M["m00"])
             cY = int(M["m01"] / M["m00"])
 
             cv2.circle(img, (cX, cY), 7, (255, 255, 255), -1)
+
+        if M2["m00"] != 0:
+            cX = int(M2["m10"] / M2["m00"])
+            cY = int(M2["m01"] / M2["m00"])
+
+            cv2.circle(img, (cX, cY), 7, (255, 0, 0), -1)
+
+        
 
     return img
 
