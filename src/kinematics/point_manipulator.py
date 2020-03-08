Changed folder and fixed broken rotation matrix

src/kinematics/point_manipulator.py
@@ -1,22 +1,36 @@
 import numpy as np
 from math import cos, sin, pi
+import time as t
 
 # Manipulates points by roll, pitch, yaw
 def rotate(pos, roll=0, pitch=0, yaw=0, rad=False):
     if not rad:                 # Converts to radians
-        roll *= pi/180.0
-        pitch *= pi/180.0
-        yaw *= pi/180.0
+        roll *= pi/180
+        pitch *= pi/180
+        yaw *= pi/180
 
     pos = np.array(pos)         # Create numpy array
 
                                 # Transformation matrix
-    r_mat = np.array([
-    [cos(yaw)*cos(pitch), cos(yaw)*sin(pitch)*sin(roll)-sin(yaw)*cos(roll), cos(yaw)*sin(pitch)*sin(roll)+sin(yaw)*cos(roll)],
-    [cos(yaw)*cos(pitch), cos(yaw)*sin(pitch)*sin(roll)+sin(yaw)*cos(roll), cos(yaw)*sin(pitch)*sin(roll)-sin(yaw)*cos(roll)],
-    [-sin(pitch), cos(pitch)*sin(roll), cos(pitch)*cos(roll)]])
+    yaw_mat = np.array([
+        [cos(yaw), -sin(yaw), 0],
+        [sin(yaw), cos(yaw), 0],
+        [0, 0, 1]
+    ])
 
-    result = np.dot(pos, r_mat).tolist()
+    pitch_mat = np.array([
+        [cos(pitch), 0, sin(pitch)],
+        [0, 1, 0],
+        [-sin(pitch), 0, cos(pitch)]
+    ])
+
+    roll_mat = np.array([
+        [1, 0, 0],
+        [0, cos(roll), -sin(roll)],
+        [0, sin(roll), cos(roll)]
+    ])
+
+    result = yaw_mat.dot(pitch_mat).dot(roll_mat).dot(pos).tolist()
     result = [round(e, 2) for e in result]
 
                                 # Return result as tuple
@@ -41,10 +55,12 @@ def translate(pos, x=0, y=0, z=0):
     
 
 if __name__ == "__main__":
-    pos = (0, 0, 1)
+    pos = (2, 0, 0)
     ang = 90
     tra = (2, 2, 2.1)
-
+    
     print(pos)
-    print(rotate(pos, roll=ang, pitch=ang, yaw=ang, rad=False))
-    print(translate(pos, *tra))
+    new_pos = rotate(pos, pitch=ang)
+    print(new_pos)
+    new_pos = translate(new_pos, *tra)
+    print(new_pos)